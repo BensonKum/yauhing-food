a9c4373c589fbe5f6a3124726cdbfdfb2d89355f
BensonKum <bensonkum@gmail.com>
2026-04-17 23:43:00 +0800

    chore: add .gitignore, remove temp debug files from tracking

--- list_images.py
+++
@@ -1,7 +0,0 @@
-import os
-
-img_dir = r'C:\Users\admin\.qclaw\workspace\yauhing-food\images'
-files = sorted(os.listdir(img_dir))
-print(f'Total images: {len(files)}')
-for f in files:
-    print(f)
